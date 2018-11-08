9dea5fd5921d0d11658ca698356ea2f0fd9224a3
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2018-11-08 23:52:11 +0000

    generalise wait loop

--- bin/setup-and-wait.sh
+++ bin/setup-and-wait.sh
@@ -2,20 +2,43 @@
 
 docker-compose up -d
 
-attempts=0
-while [ $attempts -lt 30 ]
-do
-  curl --fail http://localhost:9200/_cluster/health > /dev/null
-  if [ $? -eq 0 ]
-  then
-    echo "Elasticsearch is ready ..."
-    exit 0
-  else
-    echo "couldn't connect to elasticsearch. Retrying in one second"
-    sleep 1
-    ((attempts+=1))
-  fi
-done
+wait_for () {
+  attempts=0
+  service=$1
+  service_uri=$2
+  ready=false
 
-echo "Couldn't connect to Elasticsearch" 
-exit 1
+  if [ -z "$3" ]
+  then
+    auth=""
+  else
+    auth="-u $3"
+  fi
+
+  while [ $attempts -lt 30 ]
+  do
+    curl --silent $auth $service_uri > /dev/null
+    if [ $? -eq 0 ]
+    then
+      echo "$service is ready ..."
+      ready=true
+      break
+    else
+      echo "couldn't connect to $service. Retrying in one second"
+      sleep 1
+      ((attempts+=1))
+    fi
+  done
+
+  if $ready
+  then
+    echo "$service is ready!"
+    return
+  else
+    echo "Failed connecting to $service (max attempts reached)"
+    exit 1
+  fi
+}
+
+wait_for "elasticsearch" "http://localhost:9200/_cluster/health"
+wait_for "rabbit" "http://localhost:15672/api/vhosts" "guest:guest"
