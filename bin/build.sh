3637b2eb4ab140185504c61584f78dd6a84996bf
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2019-03-07 18:42:21 +0000

    remove alias

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
 
-alias rust-musl-builder='docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder'
-rust-musl-builder cargo build --release --target x86_64-unknown-linux-musl
+docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder cargo build --release --target x86_64-unknown-linux-musl
