44e930446d4948aff464a6255b1b52e469dde46e
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2019-03-07 19:03:29 +0000

    chown sources

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,3 @@
 #! /usr/bin/env bash
 
-sudo chown -R rust:rust src
-docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder cargo build --release --target x86_64-unknown-linux-musl
+docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder sudo chown -R rust:rust src && cargo build --release --target x86_64-unknown-linux-musl
